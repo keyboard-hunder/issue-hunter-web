777624ad2ce14231171eee8cea96b108abb32cbc
GeonLee <leegun2003@gmail.com>
2019-06-27 01:52:47 +0900

    style: header styling

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "antd";
+import { Button as AtdButton } from "antd";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between
+  width: 100%;
+  min-width: 30rem;
+  padding: 0.5rem;
+`;
 
-const Logo = styled.div``;
+const Logo = styled.div`
+  margin-right: 2rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Button = styled(AtdButton)`
+  margin: 0 0.5rem;
+`;
 
 interface State {}
 
@@ -15,8 +32,10 @@ export default class Header extends React.Component<State, Props> {
     return (
       <Container>
         <Logo>Keyboard Hunter</Logo>
-        <Button type="primary">Sign In</Button>
-        <Button>Log In</Button>
+        <ButtonContainer>
+          <Button type="primary">Sign In</Button>
+          <Button>Log In</Button>
+        </ButtonContainer>
       </Container>
     );
   }
