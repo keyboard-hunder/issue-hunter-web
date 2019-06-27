6e409fcfd3cce133940391c1d8c611d77b4e7b58
Jongwoo Na <njw1204@naver.com>
2019-06-27 23:42:31 +0900

    feat: add new block.js

--- src/block.js
+++ src/block.js
@@ -3,367 +3,414 @@ import Caver from "caver-js";
 const caver = new Caver("https://api.baobab.klaytn.net:8651/");
 
 var abi = [
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    name: "issues",
-    outputs: [
-      {
-        name: "id",
-        type: "uint256"
-      },
-      {
-        name: "owner",
-        type: "address"
-      },
-      {
-        name: "repoURL",
-        type: "string"
-      },
-      {
-        name: "issueNumber",
-        type: "uint256"
-      },
-      {
-        name: "title",
-        type: "string"
-      },
-      {
-        name: "tags",
-        type: "string"
-      },
-      {
-        name: "price",
-        type: "uint256"
-      },
-      {
-        name: "solved",
-        type: "bool"
-      },
-      {
-        name: "active",
-        type: "bool"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "github_id",
-        type: "string"
-      }
-    ],
-    name: "applyAccount",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_id",
-        type: "uint256"
-      },
-      {
-        name: "repo",
-        type: "string"
-      },
-      {
-        name: "issueNumber",
-        type: "uint256"
-      },
-      {
-        name: "title",
-        type: "string"
-      },
-      {
-        name: "tags",
-        type: "string"
-      },
-      {
-        name: "active",
-        type: "bool"
-      }
-    ],
-    name: "editIssueContents",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "address"
-      },
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    name: "issueSolvedBy",
-    outputs: [
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "renounceOwnership",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "string"
-      }
-    ],
-    name: "githubToAddress",
-    outputs: [
-      {
-        name: "",
-        type: "address"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "erc20",
-    outputs: [
-      {
-        name: "",
-        type: "address"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        name: "",
-        type: "address"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_id",
-        type: "uint256"
-      },
-      {
-        name: "price",
-        type: "uint256"
-      }
-    ],
-    name: "editIssuePrice",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "address"
-      }
-    ],
-    name: "addressToGithub",
-    outputs: [
-      {
-        name: "",
-        type: "string"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_id",
-        type: "uint256"
-      },
-      {
-        name: "by",
-        type: "address"
-      }
-    ],
-    name: "solve",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "new_erc20",
-        type: "address"
-      }
-    ],
-    name: "changeERC20",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "address"
-      },
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    name: "issueMadeBy",
-    outputs: [
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_newOwner",
-        type: "address"
-      }
-    ],
-    name: "transferOwnership",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "who",
-        type: "address"
-      },
-      {
-        name: "repo",
-        type: "string"
-      },
-      {
-        name: "issueNumber",
-        type: "uint256"
-      },
-      {
-        name: "title",
-        type: "string"
-      },
-      {
-        name: "tags",
-        type: "string"
-      },
-      {
-        name: "price",
-        type: "uint256"
-      }
-    ],
-    name: "makeIssue",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    inputs: [
-      {
-        name: "default_erc20",
-        type: "address"
-      }
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        name: "previousOwner",
-        type: "address"
-      }
-    ],
-    name: "OwnershipRenounced",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        name: "previousOwner",
-        type: "address"
-      },
-      {
-        indexed: true,
-        name: "newOwner",
-        type: "address"
-      }
-    ],
-    name: "OwnershipTransferred",
-    type: "event"
-  }
-];
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "github_id",
+             "type": "string"
+          }
+       ],
+       "name": "applyAccount",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "new_erc20",
+             "type": "address"
+          }
+       ],
+       "name": "changeERC20",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "_id",
+             "type": "uint256"
+          },
+          {
+             "name": "repo",
+             "type": "string"
+          },
+          {
+             "name": "issueNumber",
+             "type": "uint256"
+          },
+          {
+             "name": "title",
+             "type": "string"
+          },
+          {
+             "name": "tags",
+             "type": "string"
+          },
+          {
+             "name": "active",
+             "type": "bool"
+          }
+       ],
+       "name": "editIssueContents",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "_id",
+             "type": "uint256"
+          },
+          {
+             "name": "price",
+             "type": "uint256"
+          }
+       ],
+       "name": "editIssuePrice",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "who",
+             "type": "address"
+          },
+          {
+             "name": "repo",
+             "type": "string"
+          },
+          {
+             "name": "issueNumber",
+             "type": "uint256"
+          },
+          {
+             "name": "title",
+             "type": "string"
+          },
+          {
+             "name": "tags",
+             "type": "string"
+          },
+          {
+             "name": "price",
+             "type": "uint256"
+          }
+       ],
+       "name": "makeIssue",
+       "outputs": [
+          {
+             "name": "new_id",
+             "type": "uint256"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [],
+       "name": "renounceOwnership",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "_id",
+             "type": "uint256"
+          },
+          {
+             "name": "by",
+             "type": "address"
+          }
+       ],
+       "name": "solve",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "constant": false,
+       "inputs": [
+          {
+             "name": "_newOwner",
+             "type": "address"
+          }
+       ],
+       "name": "transferOwnership",
+       "outputs": [],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "function"
+    },
+    {
+       "inputs": [
+          {
+             "name": "default_erc20",
+             "type": "address"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "nonpayable",
+       "type": "constructor"
+    },
+    {
+       "anonymous": false,
+       "inputs": [
+          {
+             "indexed": true,
+             "name": "previousOwner",
+             "type": "address"
+          }
+       ],
+       "name": "OwnershipRenounced",
+       "type": "event"
+    },
+    {
+       "anonymous": false,
+       "inputs": [
+          {
+             "indexed": true,
+             "name": "previousOwner",
+             "type": "address"
+          },
+          {
+             "indexed": true,
+             "name": "newOwner",
+             "type": "address"
+          }
+       ],
+       "name": "OwnershipTransferred",
+       "type": "event"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "",
+             "type": "address"
+          }
+       ],
+       "name": "addressToGithub",
+       "outputs": [
+          {
+             "name": "",
+             "type": "string"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [],
+       "name": "erc20",
+       "outputs": [
+          {
+             "name": "",
+             "type": "address"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "githubId",
+             "type": "string"
+          }
+       ],
+       "name": "getAddressByGithubId",
+       "outputs": [
+          {
+             "name": "",
+             "type": "address"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "",
+             "type": "string"
+          }
+       ],
+       "name": "githubToAddress",
+       "outputs": [
+          {
+             "name": "",
+             "type": "address"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "",
+             "type": "address"
+          },
+          {
+             "name": "",
+             "type": "uint256"
+          }
+       ],
+       "name": "issueMadeBy",
+       "outputs": [
+          {
+             "name": "",
+             "type": "uint256"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "",
+             "type": "uint256"
+          }
+       ],
+       "name": "issues",
+       "outputs": [
+          {
+             "name": "id",
+             "type": "uint256"
+          },
+          {
+             "name": "owner",
+             "type": "address"
+          },
+          {
+             "name": "repoURL",
+             "type": "string"
+          },
+          {
+             "name": "issueNumber",
+             "type": "uint256"
+          },
+          {
+             "name": "title",
+             "type": "string"
+          },
+          {
+             "name": "tags",
+             "type": "string"
+          },
+          {
+             "name": "price",
+             "type": "uint256"
+          },
+          {
+             "name": "solved",
+             "type": "bool"
+          },
+          {
+             "name": "active",
+             "type": "bool"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "",
+             "type": "address"
+          },
+          {
+             "name": "",
+             "type": "uint256"
+          }
+       ],
+       "name": "issueSolvedBy",
+       "outputs": [
+          {
+             "name": "",
+             "type": "uint256"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [
+          {
+             "name": "repoURL",
+             "type": "string"
+          },
+          {
+             "name": "issueNumber",
+             "type": "uint256"
+          }
+       ],
+       "name": "matchedIssue",
+       "outputs": [
+          {
+             "name": "id",
+             "type": "uint256"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    },
+    {
+       "constant": true,
+       "inputs": [],
+       "name": "owner",
+       "outputs": [
+          {
+             "name": "",
+             "type": "address"
+          }
+       ],
+       "payable": false,
+       "stateMutability": "view",
+       "type": "function"
+    }
+ ];
 
-var contractAddress = "0xbbB768c31c2AD3F0aB8ca1D9ae6b333e065b8020";
+var contractAddress = "0x23C63c8EBd6249Cf75FeB2E376d4d800adc226EC";
 var myContract = new caver.klay.Contract(abi, contractAddress, {
   from: "0x1234567890123456789012345678901234567891", // default from address
   gasPrice: "25000000000" // default gas price in peb, 25 Gpeb in this case
@@ -390,34 +437,8 @@ function loadIssue(idx, callback) {
     });
 }
 
-function applyAccount(github_id) {
+export function applyAccount(github_id) {
   myContract.methods.applyAccount(github_id).send(
-    {
-      from: myAccount.address,
-      gas: "500000" // fixed
-    },
-    function(error, transactionHash) {
-      console.log(transactionHash);
-    }
-  );
-}
-
-// Server-Side Only
-function makeIssue(who, repo, issueNumber, title, tags, price) {
-  myContract.methods.makeIssue(who, repo, issueNumber, title, tags, price).send(
-    {
-      from: myAccount.address,
-      gas: "500000" // fixed
-    },
-    function(error, transactionHash) {
-      console.log(transactionHash);
-    }
-  );
-}
-
-// Server-Side Only
-function solve(id, by) {
-  myContract.methods.solve(id, by).send(
     {
       from: myAccount.address,
       gas: "500000" // fixed
